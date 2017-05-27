b31b16687d8050fbe7b2530facf5f2de3e9b00bb
Ethan Ram <ethanram@gmail.com>
2017-05-27 21:46:15 +0300

    remove out files from git

--- out/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var state_machine_1 = require("./state-machine");
-exports.StateMachine = state_machine_1.StateMachine;
-var transition_1 = require("./transition");
-exports.Transition = transition_1.Transition;
